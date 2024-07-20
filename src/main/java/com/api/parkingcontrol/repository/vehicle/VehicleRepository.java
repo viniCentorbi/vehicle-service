5a06f813ed58e564c894bd5cd0630b93f941a907
viniCentorbi <vinicentorbi@gmail.com>
2024-07-20 18:04:37 -0300

    feat: Incluir consulta paginada por tipo de veículo no Repository.

--- src/main/java/com/api/parkingcontrol/repository/vehicle/VehicleRepository.java
+++ src/main/java/com/api/parkingcontrol/repository/vehicle/VehicleRepository.java
@@ -1,10 +1,15 @@
 package com.api.parkingcontrol.repository.vehicle;
 
 import com.api.parkingcontrol.model.entity.vehicle.VehicleEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface VehicleRepository extends JpaRepository<VehicleEntity, UUID> {
 
+    @Query("SELECT v FROM VehicleEntity v WHERE v.type = ?1")
+    Page<VehicleEntity> findAllByType(int type, Pageable pageable);
 }
