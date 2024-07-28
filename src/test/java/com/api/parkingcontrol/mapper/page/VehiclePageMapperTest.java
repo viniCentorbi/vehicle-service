f1de0710d37ec04d62ea15fe5fdfb1356d460b6c
viniCentorbi <vinicentorbi@gmail.com>
2024-07-27 21:04:50 -0300

    test: Validar a utilização de uma classe genérica para mapear uma PageEntity para PageDto.

--- src/test/java/com/api/parkingcontrol/mapper/page/VehiclePageMapperTest.java
+++ src/test/java/com/api/parkingcontrol/mapper/page/VehiclePageMapperTest.java
@@ -2,8 +2,8 @@ package com.api.parkingcontrol.mapper.page;
 
 import com.api.parkingcontrol.builder.dto.vehicle.VehicleDtoBuilder;
 import com.api.parkingcontrol.builder.entity.vehicle.VehicleEntityBuilder;
+import com.api.parkingcontrol.model.dto.page.ResponsePageDto;
 import com.api.parkingcontrol.model.dto.vehicle.VehicleDto;
-import com.api.parkingcontrol.model.dto.vehicle.page.ResponseVehiclePageDto;
 import com.api.parkingcontrol.model.entity.vehicle.VehicleEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,17 +42,17 @@ class VehiclePageMapperTest {
         List<VehicleDto> expectedList = List.of(dtoBuilder.getCarDto(firstEntity), dtoBuilder.getCarDto(secondEntity),
                 dtoBuilder.getCarDto(thirdEntity));
 
-        ResponseVehiclePageDto expected = ResponseVehiclePageDto.builder()
+        ResponsePageDto<VehicleDto> expected = ResponsePageDto.<VehicleDto>builder()
                 .currentPage(0)
                 .pageSize(2)
                 .totalItems(3)
                 .totalPages(2)
-                .listVehicleDto(expectedList).build();
+                .listContent(expectedList).build();
 
         Page<VehicleEntity> pageEntity = new PageImpl<>(List.of(firstEntity, secondEntity, thirdEntity),
                 PageRequest.of(expected.getCurrentPage(), expected.getPageSize()), expected.getTotalItems());
 
-        ResponseVehiclePageDto actual = this.mapper.pageEntityToPageDto(pageEntity);
+        ResponsePageDto<VehicleDto> actual = this.mapper.pageEntityToPageDto(pageEntity);
         assertThat(actual).isNotNull().usingRecursiveComparison().isEqualTo(expected);
     }
 }
