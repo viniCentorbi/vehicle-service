6f11df9b2d8838ee3d5bf026387c04acedfdfed1
viniCentorbi <vinicentorbi@gmail.com>
2024-07-27 21:17:24 -0300

    refactor: Utilizar o mapper genérico no mapper da página de veículo.

--- src/main/java/com/api/parkingcontrol/mapper/page/VehiclePageMapper.java
+++ src/main/java/com/api/parkingcontrol/mapper/page/VehiclePageMapper.java
@@ -1,18 +1,10 @@
 package com.api.parkingcontrol.mapper.page;
 
-import com.api.parkingcontrol.model.dto.vehicle.page.ResponseVehiclePageDto;
+import com.api.parkingcontrol.model.dto.vehicle.VehicleDto;
 import com.api.parkingcontrol.model.entity.vehicle.VehicleEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.springframework.data.domain.Page;
 
 @Mapper(componentModel = "spring")
-public interface VehiclePageMapper {
+public interface VehiclePageMapper extends BasePageMapper<VehicleDto, VehicleEntity> {
 
-    @Mapping(source = "content", target = "listVehicleDto")
-    @Mapping(source = "number", target = "currentPage")
-    @Mapping(source = "size", target = "pageSize")
-    @Mapping(source = "totalPages", target = "totalPages")
-    @Mapping(source = "totalElements", target = "totalItems")
-    ResponseVehiclePageDto pageEntityToPageDto(Page<VehicleEntity> pageEntity);
 }
