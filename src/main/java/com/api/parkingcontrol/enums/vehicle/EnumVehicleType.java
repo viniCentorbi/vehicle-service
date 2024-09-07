37ea8c3e00a1189fad617dba511ad4202ea1e949
viniCentorbi <vinicentorbi@gmail.com>
2024-09-07 16:56:07 -0300

    feat: Recuperar tipo de veículo (EnumVehicleType) pelo id (String).

--- src/main/java/com/api/parkingcontrol/enums/vehicle/EnumVehicleType.java
+++ src/main/java/com/api/parkingcontrol/enums/vehicle/EnumVehicleType.java
@@ -2,15 +2,24 @@ package com.api.parkingcontrol.enums.vehicle;
 
 import lombok.Getter;
 
+import java.util.stream.Stream;
+
 @Getter
 public enum EnumVehicleType {
 
     CAR(1),
     MOTORCYCLE(2);
 
-    private final int id;
+    private final Integer id;
 
-    EnumVehicleType(int id){
+    EnumVehicleType(Integer id){
         this.id = id;
+    }
+
+    public static EnumVehicleType fromId(Integer pId){
+        return Stream.of(values())
+                .filter(item -> item.id.equals(pId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid id: " + pId));
     }
 }
