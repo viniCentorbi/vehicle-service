60d67b2e3969236a9b2331634985d3a5c2480f09
viniCentorbi <vinicentorbi@gmail.com>
2024-11-22 00:09:29 -0300

    doc: Documentar o DTO de vehicle.

--- src/main/java/com/api/vehicle/model/dto/VehicleDto.java
+++ src/main/java/com/api/vehicle/model/dto/VehicleDto.java
@@ -1,5 +1,6 @@
 package com.api.vehicle.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
@@ -16,19 +17,24 @@ public class VehicleDto {
 
     private UUID id;
 
+    @Schema(example = "Mitsubishi")
     @NotBlank
     private String brand;
 
+    @Schema(example = "Pajero Sport SE 3.0 4x2 V6 177cv Aut.")
     @NotBlank
     private String model;
 
+    @Schema(example = "Vermelho")
     @NotBlank
     private String color;
 
+    @Schema(example = "NAA4117")
     @NotBlank
     @Size(min = 7, max = 7)
     private String plate;
 
+    @Schema(example = "1", description = "1-CAR, 2-MOTORCYCLE")
     @Positive
     private int type;
 }
